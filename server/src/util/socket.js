76b129e14e15588c3e3e1e6ca8e74a8c2ac53388
Hélvio Filho <helviosvf@gmail.com>
2021-05-20 16:04:29 -0300

    Rooms controller

---
+++ server/src/util/socket.js
@@ -0,0 +1,40 @@
+import http from 'http';
+import { Server } from 'socket.io';
+
+export default class SocketServer {
+  #io
+  constructor({ port }) {
+    this.port = port;
+  }
+
+  async start() {
+    const server = http.createServer((request, response) => {
+      response.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+      });
+      response.end('hey there!!');
+    });
+
+    this.#io = new Server(server, {
+      cors: {
+        origin: '*',
+        credentials: false
+      }
+    });
+
+    // const room = this.#io.of('/room');
+    // room.on('connection', socket => {
+    //   console.log("olá");
+    //   socket.emit('userConnection', 'socket id se conectou' + socket.id);
+    //   socket.on('joinRoom', (data) => {
+    //     console.log('dados recebidos', data);
+    //   });
+    // });
+
+    return new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.listen(this.port, () => resolve(server));
+    });
+  }
+}
