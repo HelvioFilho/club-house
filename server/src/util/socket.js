c4cca7bc3ab21bed01a1b81d4db5641a6cd20c85
Hélvio Filho <helviosvf@gmail.com>
2021-05-20 16:53:40 -0300

    Adjusting generic socket

--- server/src/util/socket.js
+++ server/src/util/socket.js
@@ -1,12 +1,27 @@
 import http from 'http';
 import { Server } from 'socket.io';
+import { constants } from './constants.js';
 
 export default class SocketServer {
   #io
   constructor({ port }) {
-    this.port = port;
+    this.port = port
+    this.namespaces = {}
   }
 
+  attachEvents({ routeConfig }) {
+    for (const routes of routeConfig) {
+      for (const [namespace, { events, eventEmitter }] of Object.entries(routes)) {
+        const route = this.namespaces[namespace] = this.#io.of(`/${namespace}`)
+        route.on('connection', socket => {
+          for (const [functionName, functionValue] of events) {
+            socket.on(functionName, (...args) => functionValue(socket, ...args))
+          }
+          eventEmitter.emit(constants.event.USER_CONNECTED, socket)
+        })
+      }
+    }
+  }
   async start() {
     const server = http.createServer((request, response) => {
       response.writeHead(200, {
@@ -23,14 +38,7 @@ export default class SocketServer {
       }
     });
 
-    // const room = this.#io.of('/room');
-    // room.on('connection', socket => {
-    //   console.log("olá");
-    //   socket.emit('userConnection', 'socket id se conectou' + socket.id);
-    //   socket.on('joinRoom', (data) => {
-    //     console.log('dados recebidos', data);
-    //   });
-    // });
+
 
     return new Promise((resolve, reject) => {
       server.on('error', reject);
