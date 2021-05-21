b0d2848bc0e4d15ce113828f545a91397a65c131
Hélvio Filho <helviosvf@gmail.com>
2021-05-21 20:36:20 -0300

    createad rooms with or without topic

--- pages/lobby/src/controller.js
+++ pages/lobby/src/controller.js
@@ -1,29 +1,37 @@
 export default class LobbyController {
-  constructor({ socketBuilder, user }) {
-    this.socketBuilder = socketBuilder
-    this.user = user
+  constructor({ socketBuilder, user, view }) {
+      this.socketBuilder = socketBuilder 
+      this.user = user
+      this.view = view
 
-    this.socket = {}
+      this.socket = {}
   }
 
   static initialize(deps) {
-    return new LobbyController(deps)._init()
+      return new LobbyController(deps)._init()
   }
 
   async _init() {
-    this.socket = this._setupSocket()
+      this._setupViewEvents()
+      this.socket = this._setupSocket()
   }
 
-  _setupSocket() {
-      this.socketBuilder
-      .setOnLobbyUpdated(this.onLobbyUpdated())
+  _setupViewEvents() {
+      this.view.updateUserImage(this.user)
+      this.view.configureCreateRoomButton()
       
+  }
+  _setupSocket() {
+      return this.socketBuilder
+      .setOnLobbyUpdated(this.onLobbyUpdated())
+      .build()
   }
 
   onLobbyUpdated() {
-    return (rooms) => {
-      console.log('rooms', rooms)
-    }
+      return (rooms) => { 
+          console.log('rooms', rooms)
+          this.view.updateRoomList(rooms)
+      }
   }
-
 }
+
---
+++ pages/lobby/src/index.js
@@ -0,0 +1,21 @@
+import { constants } from "../../_shared/constants.js";
+import LobbyController from "./controller.js";
+import LobbySocketBuilder from "./util/lobbySocketBuilder.js";
+import View from "./view.js";
+
+const user = {
+  img: 'https://cdn3.iconfinder.com/data/icons/user-avatars-1/512/users-10-3-512.png',
+  username: 'Hélvio ' + Date.now()
+}
+
+const socketBuilder = new LobbySocketBuilder({
+  socketUrl: constants.socketUrl,
+  namespace: constants.socketNamespaces.lobby
+})
+
+const dependencies = {
+  socketBuilder,
+  user,
+  view: View
+}
+await LobbyController.initialize(dependencies)
