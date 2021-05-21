f0fe0f19f4d772c847cbbab1037d0193dd9f84fc
Hélvio Filho <helviosvf@gmail.com>
2021-05-21 15:48:12 -0300

    Added and updated room users

--- pages/room/src/controller.js
+++ pages/room/src/controller.js
@@ -1,40 +1,69 @@
 import { constants } from "../../_shared/constants.js";
+import Attendee from "./entities/attendee.js";
 
 export default class RoomController {
-  constructor({ roomInfo, socketBuilder }) {
-    this.socketBuilder = socketBuilder;
-    this.roomInfo = roomInfo;
+    constructor({ roomInfo, socketBuilder, view }) {
+        this.socketBuilder = socketBuilder;
+        this.roomInfo = roomInfo;
+        this.view = view;
+        
+        this.socket = {};
+    }
+    static async initialize(deps) {
+        return new RoomController(deps)._initialize();
+    }
 
-    this.socket = {}
-  }
+    async _initialize() {
+        this._setupViewEvents();
+        this.socket = this._setupSocket();
+        this.socket.emit(constants.events.JOIN_ROOM, this.roomInfo);
+    }
 
-  static async initialize(deps) {
-    return new RoomController(deps)._initialize();
-  }
+    _setupViewEvents() {
+        this.view.updateUserImage(this.roomInfo.user);
+        this.view.updateRoomTopic(this.roomInfo.room);
+    }
 
-  async _initialize() {
-    
-    this.socket = this._setupSocket()
-    this.socket.emit(constants.events.JOIN_ROOM, this.roomInfo);
-  }
-  
-  _setupSocket(){
-    return  this.socketBuilder
-      .setOnUserConnected(this.onUserConnected())
-      .setOnUserDisconnected(this.onUserDisconnected())
-      .setOnRoomUpdated(this.onRoomUpdated())
-      .build();
-  }
+    _setupSocket() {
+        return this.socketBuilder
+            .setOnUserConnected(this.onUserConnected())
+            .setOnUserDisconnected(this.onDisconnected())
+            .setOnRoomUpdated(this.onRoomUpdated())
+            .setOnUserProfileUpgrade(this.onUserProfileUpgrade())
+            .build();
+    }
 
-  onRoomUpdated() {
-    return (room) => console.log('room list!', room);
-  }
+    onUserProfileUpgrade() {
+        return (data) => {
+            const attendee = new Attendee(data);
+            console.log('onUserProfileUpgrade', attendee);
+            if(attendee.isSpeaker) {
+                this.view.addAttendeeOnGrid(attendee, true);
+            }
+         }
+    }
 
-  onUserDisconnected() {
-    return (user) => console.log('user disconnected', user);
-  }
+    onRoomUpdated() {
+        return (room) => {
+            this.view.updateAttendeesOnGrid(room);
+            console.log('room list!', room);
+        }
+    }
 
-  onUserConnected() {
-    return (user) => console.log('user connected', user);
-  }
+    onDisconnected() {
+        return (data) => {
+            const attendee = new Attendee(data);
+
+            console.log(`${attendee.username} disconnected!`);
+            this.view.removeItemFromGrid(attendee.id);
+        }
+    }
+
+    onUserConnected() {
+        return (data) => { 
+            const attendee = new Attendee(data);
+            console.log('user connected!', attendee);
+            this.view.addAttendeeOnGrid(attendee);
+        }
+    }
 }
--- pages/room/src/util/roomSocket.js
+++ pages/room/src/util/roomSocket.js
@@ -3,19 +3,27 @@ import { constants } from "../../../_shared/constants.js";
 import SocketBuilder from "../../../_shared/socketBuilder.js";
 
 export default class RoomSocketBuilder extends SocketBuilder {
-   constructor( {socketUrl, namespace}) {
-     super({socketUrl, namespace});
-     this.onRoomUpdated = () => {}
+  constructor({ socketUrl, namespace }) {
+    super({ socketUrl, namespace })
+    this.onRoomUpdated = () => { }
+    this.onUserProfileUpgrade = () => { }
   }
 
-  setOnRoomUpdated(fn){
+  setOnRoomUpdated(fn) {
     this.onRoomUpdated = fn;
     return this;
   }
 
-  build(){
-    const socket = super.build()
-    socket.on(constants.events.LOBBY_UPDATED, this.onRoomUpdated)
+  setOnUserProfileUpgrade(fn) {
+    this.onUserProfileUpgrade = fn;
+    return this;
+  }
+
+  build() {
+    const socket = super.build();
+    socket.on(constants.events.LOBBY_UPDATED, this.onRoomUpdated);
+    socket.on(constants.events.UPGRADE_USER_PERMISSION, this.onUserProfileUpgrade);
+
     return socket;
   }
 }
