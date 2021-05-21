14ffad9a15f5df575aa9afa9734db17472fd60fe
Hélvio Filho <helviosvf@gmail.com>
2021-05-21 14:49:40 -0300

    Controller Room

--- pages/room/src/index.js
+++ pages/room/src/index.js
@@ -1,25 +1,27 @@
 import { constants } from "../../_shared/constants.js"
+import RoomController from "./controller.js"
 import RoomSocketBuilder from "./util/roomSocket.js"
-
-const socketBuilder = new RoomSocketBuilder({
-  socketUrl: constants.socketUrl,
-  namespace: constants.socketNamespaces.room
-})
-
-const socket = socketBuilder
-  .setOnUserConnected((user) => console.log('user connected', user))
-  .setOnUserDisconnected((user) => console.log('user disconnected', user))
-  .setOnRoomUpdated((room) => console.log('room list!', room))
-  .build()
 
 const room = {
   id: '0001',
   topic: 'Js Expert'
-}
+};
 
 const user = {
   img: 'https://cdn3.iconfinder.com/data/icons/user-avatars-1/512/users-10-3-512.png',
   username: 'Hélvio Filho' + Date.now(),
+};
+
+const roomInfo = { user, room };
+
+const socketBuilder = new RoomSocketBuilder({
+  socketUrl: constants.socketUrl,
+  namespace: constants.socketNamespaces.room
+});
+
+const dependencies = {
+  socketBuilder,
+  roomInfo
 }
 
-socket.emit(constants.events.JOIN_ROOM, {user, room})
+await RoomController.initialize(dependencies);
